c29c2bb143a3c0bd218046ba72afaf712cd18d27
Andrei Sirotenko <panshirotenski@gmail.com>
2025-01-03 03:15:56 +0300

    Day25 2024

---
+++ 2024/AoC2024/Day25.cs
@@ -0,0 +1,85 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace AoC2024;
+
+public class Day25
+{
+    [Theory]
+    [MemberData(nameof(TestCases1))]
+    public void Part1(string input, int expected)
+    {
+        var parts = input.Split(["\r\n\r\n", "\n\n"], StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.Split(["\r\n", "\n"], StringSplitOptions.RemoveEmptyEntries))
+            .ToArray();
+        var locks = parts.Where(x => x.First().All(y => y == '#')).ToArray();
+        var keys = parts.Where(x => x.Last().All(y => y == '#')).ToArray();
+        var ret = 0;
+        foreach(var k in keys) {
+            foreach(var l in locks) {
+                var n = k.Length;
+                var m = k.First().Length;
+                if (Enumerable.Range(0, n *m ).All(i => k[i / m][i % m] == '.' ||  l[i / m][i % m] == '.')) {
+                    ret++;
+                }
+            }
+        }
+
+        ret.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> TestCases1 =>
+       new List<object[]>
+       {
+            new object[]
+                        {
+                            """
+                            #####
+                            .####
+                            .####
+                            .####
+                            .#.#.
+                            .#...
+                            .....
+
+                            #####
+                            ##.##
+                            .#.##
+                            ...##
+                            ...#.
+                            ...#.
+                            .....
+
+                            .....
+                            #....
+                            #....
+                            #...#
+                            #.#.#
+                            #.###
+                            #####
+
+                            .....
+                            .....
+                            #.#..
+                            ###..
+                            ###.#
+                            ###.#
+                            #####
+
+                            .....
+                            .....
+                            .....
+                            #....
+                            #.#..
+                            #.#.#
+                            #####
+                            """,
+                            3
+                        },
+            new object[]
+                        {
+                            File.ReadAllText("Inputs/Day25.txt"),
+                            3301
+                        },
+    };
+}
